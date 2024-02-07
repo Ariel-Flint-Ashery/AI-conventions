53f1d7ff4ada3dce46a173a1d933e09a7053bba7
Ariel Flint Ashery <af919@ic.ac.uk>
2024-02-07 13:22:07 +0000

    updated conventions

--- conventions.py
+++ conventions.py
@@ -17,7 +17,7 @@ model_dir = 'meta-llama/Llama-2-7b-chat-hf'
 model = AutoModelForCausalLM.from_pretrained(model_dir, torch_dtype=torch.float16,
     device_map="auto",)
 tokenizer = AutoTokenizer.from_pretrained(model_dir)
-
+print("MODEL LOADED")
 """Before we proceed, it will be useful for us to create a function that automatically formats a given prompt into the form that can be read by the Llama 2 model. This script is taken from https://github.com/facebookresearch/llama-recipes/blob/main/src/llama_recipes/inference/chat_utils.py ."""
 
 B_INST, E_INST = "[INST]", "[/INST]"
@@ -76,12 +76,13 @@ from string import ascii_lowercase as alphabet
 from collections import defaultdict
 
 """**LOAD SIMULATION PARAMETERS**"""
+network_type = 'complete'
 iterations=10
 pool_size = 6
-word_size = 3
+word_size = 6
 r=20
-N = 24 #number of agents
-network = {n+1: {'my_history': [], 'partner_history': [], 'interactions': [], 'score': 0, 'score_history': []} for n in range(N)}
+N = 48 #number of agents
+#network = {n+1: {'my_history': [], 'partner_history': [], 'interactions': [], 'score': 0, 'score_history': []} for n in range(N)}
 
 """**GAME SETUP**
 
@@ -92,11 +93,11 @@ def get_pool(default_pool = True):
   pool = str()
   while len(set(pool)) <= 1:
     if default_pool == True:
-      pool = ''.join(random.choices('fj', k = pool_size))
-
+      # pool = ''.join(random.choices('fj', k = pool_size))
+      pool = 'fj'
     else:
-      pool = ''.join(random.choices(alphabet, k = pool_size))
-
+      #pool = ''.join(random.choices(alphabet, k = pool_size))
+      pool = ''.join(random.choices('fj', k = pool_size))
   return pool
 
 def recover_string(answer):
@@ -120,17 +121,17 @@ while incorrect_example == correct_example:
 
 rules = f"""You are playing a game repeatedly with another player. In this game, you are presented with a pool of letters: '{pool}'.
 The rules of the game are as follows:
-1. You must produce a three character string from the pool.
-2. If you and the other player produce matching stringd, then you win 10 points. For example: "In round X, you produced '{correct_example}' and the other player produced '{correct_example}'. Thus you won 10 points."
-3. If you produce a string that does not match the other player's string, then you lose 5 points. For example: "In round X, you produced '{correct_example}' and the other player produced '{incorrect_example}'. Thus you lost 5 points."
+1. You must produce a six character string from the pool.
+2. If you and the other player produce matching strings, then you win 10 points. For example: "In round Y, you produced '{correct_example}' and the other player produced '{correct_example}'. Thus you won 10 points."
+3. If you produce a string that does not match the other player's string, then you lose 5 points. For example: "In round Y, you produced '{correct_example}' and the other player produced '{incorrect_example}'. Thus you lost 5 points."
 4. Your aim is to maximise your own point count.
-5. Your answer must be of the form "I will choose the string 'XXX'.".
+5. Your answer must be of the form "I will choose the string 'XXXXXX'.".
 6. The minimum possible point tally is 0. If you lose points when your tally is 0, then the tally remains unchanged."""
 #print(rules)
 
 """Now, choose your initial round."""
 
-initial_round = "In round 1, you produced 'fjf' and the other player produced 'jjj'. Thus you lost 5 points."
+initial_round = "In round 1, you produced 'ffffff' and the other player produced 'jjjjjj'. Thus you lost 5 points."
 
 """We format our prompts by a method prescribed by Llama team"""
 
@@ -180,8 +181,8 @@ def get_llama_response(prompt):
 First, let us build the network interaction structure. We assume a random regular graph.
 """
 
-def get_interaction_network(network_dict, network_type = 'complete', degree=4, rounds = 10, alpha = 0.41, beta=0.54, p=0.5):
-
+def get_interaction_network(network_type = 'complete', degree=4, rounds = 10, alpha = 0.41, beta=0.54, p=0.5):
+  network_dict = {n+1: {'my_history': [], 'partner_history': [], 'interactions': [], 'score': 0, 'score_history': []} for n in range(N)}
   if network_type == 'random_regular':
     graph = nx.random_regular_graph(d=degree, n=len(network_dict.keys()))
     for n in network_dict.keys():
@@ -238,25 +239,26 @@ def get_prompt(network_dict, p, initial_round = initial_round):
 
   # add initial round
   histories = [{"role": "system", "content": rules},
-        {"role": "user", "content": "You are currently playing in round 1. Your point tally is 0. Q: What three character string would you like to produce from the available pool?"},
-        {"role": "assistant", "content": "I will choose the string 'fjf'." },
-        {"role": "user", "content": initial_round + "You are currently playing in round 2. Your point tally is 0. Q: What three character string would you like to produce from the available pool?"},
+        {"role": "user", "content": "You are currently playing in round 1. Your point tally is 0. Q: What six character string would you like to produce from the available pool?"},
         ]
+        #{"role": "assistant", "content": "I will choose the string 'ffffff'." },
+        #{"role": "user", "content": initial_round + "You are currently playing in round 2. Your point tally is 0. Q: What six character string would you like to produce from the available pool?"},
+        #]
 
   # load history statements
   # for i,r in enumerate(player['interactions'].keys()): #r is the global round, i is the player's local round
   #   if r == current_round:
   #     break
-  for idx,_ in enumerate(player['interactions']):
+  for idx in range(len(player['my_history'])):
     my_answer = player['my_history'][idx] #r
     partner_answer = player['partner_history'][idx] #r
     outcome = get_outcome(my_answer, partner_answer)
     histories.append({"role": "assistant", "content": f"I will choose the string {my_answer}."})
     if outcome > 0: # match
-      histories.append({"role": "user", "content": f"In round {idx+2}, you produced {my_answer} and the other player produced {partner_answer}. Thus you won {outcome} points. You are currently playing in round {idx+3}. Your point tally is {player['score']}. Q: What three character string would you like to produce from the available pool?"})
+      histories.append({"role": "user", "content": f"In round {idx+1}, you produced {my_answer} and the other player produced {partner_answer}. Thus you won {outcome} points. You are currently playing in round {idx+2}. Your point tally is {player['score_history'][idx]}. Q: What six character string would you like to produce from the available pool?"})
 
     else: # no match
-      histories.append({"role": "user", "content": f"In round {idx+2}, you produced {my_answer} and the other player produced {partner_answer}. Thus you lost {outcome} points. You are currently playing in round {idx+3}. Your point tally is {player['score']}. Q: What three character string would you like to produce from the available pool?"})
+      histories.append({"role": "user", "content": f"In round {idx+1}, you produced {my_answer} and the other player produced {partner_answer}. Thus you lost {outcome} points. You are currently playing in round {idx+2}. Your point tally is {player['score_history'][idx]}. Q: What six character string would you like to produce from the available pool?"})
 
   return histories
 
@@ -278,6 +280,12 @@ def update_dict(network_dict, player, my_answer, partner_answer, outcome):
   network_dict[player]['my_history'].append(my_answer)
   network_dict[player]['partner_history'].append(partner_answer)
   network_dict[player]['score_history'].append(network_dict[player]['score'])
+
+def set_initial_state(network_dict, my_answer, partner_answer):
+    outcome = get_outcome(my_answer, partner_answer)
+    for p in network_dict.keys():
+        update_dict(network_dict, p, my_answer, partner_answer, outcome)
+        
 
 # def simulation_old(network_dict, network_type = 'complete', degree=4, rounds=10):
 #   """
@@ -307,13 +315,24 @@ def update_dict(network_dict, player, my_answer, partner_answer, outcome):
 #       update_dict(interaction_dict, p2, r, partner_answer, my_answer, outcome)
 
 #   return interaction_dict
+def update_tracker(tracker, p1, p2, p1_answer, p2_answer, outcome):
+  tracker['players'].append([p1, p2])
+  tracker['p1'].append(p1_answer)
+  tracker['p2'].append(p2_answer)
+  if outcome > 0:
+    tracker['outcome'].append(1)
+  else:
+    tracker['outcome'].append(0)
 
-def simulation(network_dict, network_type='complete', rounds=10, *params):
-  interaction_dict = get_interaction_network(network_dict, network_type = network_type, **params)
 
-  while min([len(interaction_dict[p]['interactions'].keys()) for p in interaction_dict.keys()]) < rounds:
+def simulation(network_type='complete', rounds=10):
+  interaction_dict = get_interaction_network(network_type = network_type)
+  tracker = {'players': [], 'p1': [], 'p2': [], 'outcome': []}
+  set_initial_state(interaction_dict, 'ffffff', 'jjjjjj')
+  while min([len(interaction_dict[p]['interactions']) for p in interaction_dict.keys()]) < rounds:
     #randomly choose player and a neighbour
-    p1 = random.choice(interaction_dict.keys())
+    #print('simulation function called')
+    p1 = random.choice(list(interaction_dict.keys()))
     p2 = random.choice(interaction_dict[p1]['neighbours'])
     #add interactions to play history
     interaction_dict[p1]['interactions'].append(p2)
@@ -328,21 +347,24 @@ def simulation(network_dict, network_type='complete', rounds=10, *params):
     outcome = get_outcome(my_answer, partner_answer)
     update_dict(interaction_dict, p1, my_answer, partner_answer, outcome)
     update_dict(interaction_dict, p2, partner_answer, my_answer, outcome)
+    update_tracker(tracker, p1, p2, my_answer, partner_answer, outcome)
 
-  return interaction_dict
+  return interaction_dict, tracker
 
 
 """**RUNNING THE SIMULATION**
 
 We are now ready to test the simulation! Let us see how it works.
 """
-dataframe = {it: {} for it in range(iterations)}
+print('STARTING SIMULATIONS')
+dataframe = {it: {'simulation': {}, 'tracker': {}} for it in range(iterations)}
 for it in range(iterations):
-  network = {n+1: {'my_history': {}, 'partner_history': {}, 'interactions': defaultdict(int), 'score': 0, 'score_history': {}} for n in range(N)}
-  simulation_dict = simulation(network, network_type = 'complete', degree = 4, rounds = r)
-  dataframe[it] = simulation_dict
-
-fname = f'{pool}_{N}ps_{iterations}its_{r}rds.pkl'
+  #network = {n+1: {'my_history': [], 'partner_history': [], 'interactions': [], 'score': 0, 'score_history': []} for n in range(N)}
+  simulation_dict, tracker_dict = simulation(network_type = network_type, rounds = 80)
+  dataframe[it]['simulation'] = simulation_dict
+  dataframe[it]['tracker'] = tracker_dict
+  print(f'SIMULATION {it} COMPLETED')
+fname = f'{network_type}_{pool}_{N}ps_{iterations}its_{r}rds.pkl'
 f = open(fname, 'wb')
 pickle.dump(dataframe, f)
 f.close()
